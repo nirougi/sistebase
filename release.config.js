b2ad55674f2fd0a01ee1b97457c4c439d403c707
Nicolás Rougier <jinik.nr@gmail.com>
2020-06-27 14:32:33 -0300

    Agrega semantic release

---
+++ release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  release: {
+    branches: ["master"]
+  },
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/npm",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/github",
+      {
+        assets: ["dist/**", "LICENSE", "README.md"]
+      }
+    ]
+  ]
+};
