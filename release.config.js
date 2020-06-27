0a91a8d8d5ad31306d2a98fca169d0c2a99b240c
Nicolás Rougier <jinik.nr@gmail.com>
2020-06-27 16:31:37 -0300

    chore: Modifica la config de los assets en el release

--- release.config.js
+++ release.config.js
@@ -1,16 +1,5 @@
 module.exports = {
   release: {
     branches: ["master"]
-  },
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/npm",
-    "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/github",
-      {
-        assets: ["dist/**", "LICENSE", "README.md", "package.json"]
-      }
-    ]
-  ]
+  }
 };
